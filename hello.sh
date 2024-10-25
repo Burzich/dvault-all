2ef430985391c8b5f2b93a7074786c553ad2f680
serg-shkviro <serg200211@mail.ru>
2024-10-25 04:21:50 +0300

    Add hello

--- hello.sh
+++ hello.sh
@@ -1,55 +1,61 @@
-# Debichыы - Vault Main
+#!/bin/bash
 
-Мы не успели дописать автоматическое создание сертификатов, поэтому создать надо вручную:
+if [ ! -d "cert_postgresql" ]; then
+  echo "Создание директории cert_postgresql..."
+  mkdir cert_postgresql
+fi
 
-Генерация сертификатов 
+cd cert_postgresql || exit
 
-```bash
-cd cert_postgresql
+echo "Генерация ключа и сертификата CA для PostgreSQL..."
 openssl genpkey -algorithm RSA -out ca-postgresql.key
 openssl req -x509 -new -nodes -key ca-postgresql.key -days 3650 -out ca-postgresql.crt -subj "/CN=MyPostgresCA"
 
-
+echo "Генерация ключа и сертификата для PostgreSQL сервера..."
 openssl genpkey -algorithm RSA -out postgresql-server.key
 openssl req -new -key postgresql-server.key -out postgresql-server.csr -subj "/CN=postgresql-server"
 openssl x509 -req -in postgresql-server.csr -CA ca-postgresql.crt -CAkey ca-postgresql.key -CAcreateserial -out postgresql-server.crt -days 365
 
+echo "Генерация ключа и сертификата для PostgreSQL клиента..."
 openssl genpkey -algorithm RSA -out postgresql-client.key
 openssl req -new -key postgresql-client.key -out postgresql-client.csr -subj "/CN=postgresql-client"
 openssl x509 -req -in postgresql-client.csr -CA ca-postgresql.crt -CAkey ca-postgresql.key -CAcreateserial -out postgresql-client.crt -days 365
-```
 
-```bash
-cd ..
-sudo chown postgres:postgres ./cert_postgresql/*
 
-cd /redis/cert_redis
+echo "Изменение прав на сертификаты PostgreSQL..."
+sudo chown root:root ./*
+sudo chmod 600 postgresql-server.key
+
+cd .. || exit
+
+if [ ! -d "cert_redis" ]; then
+  echo "Создание директории cert_redis..."
+  mkdir -p cert_redis
+fi
+
+cd cert_redis || exit
+
+echo "Генерация ключа и сертификата CA для Redis..."
 openssl genpkey -algorithm RSA -out ca.key
 openssl req -x509 -new -nodes -key ca.key -days 3650 -out ca.crt -subj "/CN=MyRedisCA"
 
-
+echo "Генерация ключа и сертификата для Redis сервера..."
 openssl genpkey -algorithm RSA -out redis-server.key
 openssl req -new -key redis-server.key -out redis-server.csr -subj "/CN=redis-server"
 openssl x509 -req -in redis-server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out redis-server.crt -days 365
 
+echo "Генерация ключа и сертификата для Redis клиента..."
 openssl genpkey -algorithm RSA -out redis-client.key
 openssl req -new -key redis-client.key -out redis-client.csr -subj "/CN=redis-client"
 openssl x509 -req -in redis-client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out redis-client.crt -days 365
-```
 
-```bash
-sudo chown redis:redis ./cert_redis/*
-```
+echo "Изменение прав на сертификаты Redis..."
+sudo chown root:root ./*
 
-После поднимаем все сервисы
+cd .. || exit
 
-
-
-```bash
+echo "Запуск Docker Compose..."
 docker compose up -d
-```
 
-
-Также, выполним `./test_postgresql.sh` для внесения сертификатов в postgres. 
-
-На выходе получаем готовый backend.
+echo "Запуск тестового скрипта для PostgreSQL..."
+./test_postgresql.sh
